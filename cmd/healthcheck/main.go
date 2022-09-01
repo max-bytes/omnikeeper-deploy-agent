3a053673651a14a669192f188446d82b69cbc444
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-09-01 18:02:11 +0200

    preparations for healthcheck support

--- cmd/healthcheck/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"os"
-)
-
-func main() {
-	os.Exit(1) // TODO
-}
---
+++ pkg/healthcheck/healthcheck.go
@@ -0,0 +1,11 @@
+package healthcheck
+
+import "os"
+
+func Check() {
+	os.Exit(1) // TODO
+}
+
+func TouchStatsFile() {
+	// TODO
+}
